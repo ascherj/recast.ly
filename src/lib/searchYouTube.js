55bd4f9e2337d5f29b9baa70ff2c332526b0c0e2
Jake Ascher <ascherj@gmail.com>
2019-01-17 19:08:17 -0500

    Implement searchYouTube function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      maxResults: options.max,
+      q: options.query,
+      type: 'video',
+      key: options.key,
+      videoEmbeddable: true
+    },
+    dataType: 'json',
+    success: (data) => {
+      callback(data.items);
+    },
+    error: () => {
+      console.error('youtube: Failed to get videos');
+    }
+  });
 };
 
 export default searchYouTube;
