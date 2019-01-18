5a296ac69962a90b7fe4c747deeb60070fa98b52
Jake Ascher <ascherj@gmail.com>
2019-01-17 20:31:08 -0500

    Add a semicolon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import App from './components/App.js';
-import searchYouTube from './lib/searchYouTube.js'
+import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 
 ReactDOM.render(<App searchYouTube={searchYouTube}/>, document.getElementById('app'));
